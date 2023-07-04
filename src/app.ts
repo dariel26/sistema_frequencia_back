07be7f909d2ca0bd380436146c2871c0a7fadbb2
dariel26 <asdfgh.daac@gmail.com>
2023-07-04 04:21:33 -0300

    Modificando view do BD e aumentando limite de dado por requisicao

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ import apiV1 from "./routes/api/apiV1";
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(bodyParser.urlencoded({ extended: false, limit: "10mb" }));
 app.use(cors());
 app.use((_, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
