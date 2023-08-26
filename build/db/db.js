70d68e9c7258b970ad6d6395769392f8812fde42
dariel26 <dariel.arian18@gmail.com>
2023-08-26 13:24:41 -0300

    Mudança em dados do bd
    Mudança da variável de ambiente NODE_ENV influencia qual banco de
    dados será usado

--- build/db/db.js
+++ build/db/db.js
@@ -3,18 +3,46 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
 const mysql2_1 = __importDefault(require("mysql2"));
+const ambiente = process.env.NODE_ENV;
+const host = ambiente === "pro"
+    ? process.env.DB_HOST_PRO
+    : ambiente === "hom"
+        ? process.env.DB_HOST_HOM
+        : process.env.DB_HOST_DEV;
+const user = ambiente === "pro"
+    ? process.env.DB_USER_PRO
+    : ambiente === "hom"
+        ? process.env.DB_USER_HOM
+        : process.env.DB_USER_DEV;
+const pass = ambiente === "pro"
+    ? process.env.DB_PASS_PRO
+    : ambiente === "hom"
+        ? process.env.DB_PASS_HOM
+        : process.env.DB_PASS_DEV;
+const data = ambiente === "pro"
+    ? process.env.DB_DATA_PRO
+    : ambiente === "hom"
+        ? process.env.DB_DATA_HOM
+        : process.env.DB_DATA_DEV;
+const port = ambiente === "pro"
+    ? process.env.DB_PORT_PRO
+    : ambiente === "hom"
+        ? process.env.DB_PORT_HOM
+        : process.env.DB_PORT_DEV;
 const pool = mysql2_1.default.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
-    port: 3306,
+    host,
+    user,
+    password: pass,
+    database: data,
+    port: parseInt(port !== null && port !== void 0 ? port : "3306"),
     waitForConnections: true,
     connectionLimit: 200,
-    idleTimeout: 60000,
+    idleTimeout: 7000,
     queueLimit: 0,
 });
 const db = pool.promise();
+if (ambiente === "dev") {
+    console.log(`Conectado ao banco de dados com sucesso, dados: DB_HOST: ${host} DB_USER: ${user} DB_PASS: ${pass} DB_DATA: ${data} DB_PORT: ${port}`);
+}
 exports.default = db;
